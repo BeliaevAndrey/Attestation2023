86fb47dc8c70f082173b87372a76789318f26903
Andrei Beliaev <abbot190414@gmail.com>
2023-02-22 02:46:43 +0300

    tasks-13-15: Classes added

---
+++ JAVA/ZooRegistry/src/main/java/org/example/classes/Sumpters.java
@@ -0,0 +1,9 @@
+package org.example.classes;
+
+import java.time.LocalDate;
+
+public abstract class Sumpters extends Animal{
+    Sumpters(String Name, LocalDate BirthDay) {
+        super(Name, BirthDay);
+    }
+}
